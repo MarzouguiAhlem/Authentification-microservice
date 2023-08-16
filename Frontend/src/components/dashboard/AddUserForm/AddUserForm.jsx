82acc990dd5f105ffcfe68b798678545fa6a836f
Ah Mar <marzouguiahlem9121@gmail.com>
2023-08-16 19:25:05 +0100

    adding the frontend to establich the connection

--- Frontend/src/components/dashboard/AddUserForm/AddUserForm.jsx
+++ Frontend/src/components/dashboard/AddUserForm/AddUserForm.jsx
@@ -3,32 +3,38 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
-import './Signup.css'
 import * as yup from "yup";
 import * as formik from "formik";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { setMsg } from "../../features/redux/appSlice";
-import { setSignupEmail, userSignup } from "../../features/redux/userSlice";
+import { setMsg } from "../../../features/redux/appSlice";
+import { userSignup } from "../../../features/redux/userSlice";
 import { useNavigate } from "react-router-dom";
-import { relativePaths } from '../../navigation';
+import { AddUser } from "../../../features/redux/adminDashboardSlice";
 
-
-const SignupForm = () => {
-  const { Formik } = formik;
+const AddUserForm = () => {
+  const { Formik, Field } = formik;
 
   const schema = yup.object().shape({
-    firstName: yup.string().required().min(3).max(20).matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
-    lastName: yup.string().required().min(2).max(20).matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
+    firstName: yup
+      .string()
+      .required()
+      .min(3)
+      .max(20)
+      .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
+    lastName: yup
+      .string()
+      .required()
+      .min(2)
+      .max(20)
+      .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
     email: yup.string().required().email().max(50),
-    password: yup.string().required().min(4).max(50),
-    confirmPassword: yup.string().required().min(4).max(50).oneOf([yup.ref('password'), null], 'Passwords must match'),
     adress: yup.string().required().max(200),
-    terms: yup.bool().required().oneOf([true], "Terms must be accepted"),
+    role: yup.string().required(),
   });
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const { msg, msgType, isLoading } = useSelector((state) => state.app);
 
@@ -54,32 +60,29 @@ const SignupForm = () => {
           pauseOnHover: true,
           draggable: true,
           progress: undefined,
-          });
+        });
       }
-      dispatch(setMsg(""))
+      dispatch(setMsg(""));
     }
   }, [msg]);
 
   return (
     <div className="container mt-5">
+      <h3>Add User/Admin</h3>
       <Formik
         validationSchema={schema}
         onSubmit={(values, actions) => {
-          let {confirmPassword,terms,...user} = values
-          console.log(user)
-          dispatch(userSignup({user:user, navigate:navigate}))
-          // dispatch(userSignup(user))
-          // .then(()=>navigate(relativePaths.verifyEmail))
-          actions.setSubmitting(true);
+          let user = values;
+          console.log(user);
+          dispatch(AddUser(user));
+          actions.setSubmitting(false);
           actions.resetForm({
             values: {
               firstName: "",
               lastName: "",
               email: "",
-              password:"",
-              confirmPassword:"",
               adress: "",
-              terms: false,
+              role: "",
             },
           });
         }}
@@ -87,10 +90,8 @@ const SignupForm = () => {
           firstName: "",
           lastName: "",
           email: "",
-          password:"",
-          confirmPassword:"",
           adress: "",
-          terms: false,
+          role: "",
         }}
       >
         {({
@@ -103,11 +104,7 @@ const SignupForm = () => {
           errors,
           handleReset,
         }) => (
-          <Form noValidate onSubmit={(e)=>{
-            e.preventDefault()
-            handleSubmit(e)
-            e.preventDefault()
-            }}>
+          <Form noValidate onSubmit={handleSubmit}>
             <Row className="mb-3">
               <Col md={{ span: 10, offset: 1 }}>
                 <Form.Group controlId="validationFormik01">
@@ -179,47 +176,6 @@ const SignupForm = () => {
                 </Form.Group>
 
                 <FloatingLabel
-                  controlId="validationFormik04"
-                  label="Password"
-                  className="mb-3"
-                >
-                  <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    isValid={touched.password && !errors.password}
-                    isInvalid={touched.password && !!errors.password}
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    {errors.password}
-                  </Form.Control.Feedback>
-                </FloatingLabel>
-
-                <FloatingLabel
-                  controlId="validationFormik05"
-                  label="Confirm Password"
-                  className="mb-3"
-                >
-                  <Form.Control
-                    type="password"
-                    placeholder="Confirm Password"
-                    name="confirmPassword"
-                    value={values.confirmPassword}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    isValid={touched.confirmPassword && !errors.confirmPassword}
-                    isInvalid={touched.confirmPassword && !!errors.confirmPassword}
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    {errors.confirmPassword}
-                  </Form.Control.Feedback>
-                </FloatingLabel>
-
-
-                <FloatingLabel
                   controlId="validationFormik06"
                   label="Adress"
                   className="mb-3"
@@ -239,21 +195,30 @@ const SignupForm = () => {
                   </Form.Control.Feedback>
                 </FloatingLabel>
 
-                <Form.Group className="mb-3">
-                  <Form.Check
-                    required
-                    name="terms"
-                    label="Agree to terms and conditions"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    isInvalid={!!errors.terms && touched.terms}
-                    feedback={errors.terms}
-                    feedbackType="invalid"
-                    id="validationFormik07"
-                  />
-                </Form.Group>
+                <div role="group" aria-labelledby="my-radio-group">
+                  <label>
+                    <Field type="radio" name="role" value="user" />
+                    User
+                  </label>
+                  <br />
+                  <label>
+                    <Field type="radio" name="role" value="admin" />
+                    Admin
+                  </label>
+                  <p
+                    style={{
+                      color: "#ffa4a4",
+                      letterSpacing: "0.1em",
+                      fontSize: "0.875em",
+                    }}
+                  >
+                    {errors.role}
+                  </p>
+                </div>
                 <div className="btn-submit">
-                    <Button variant="danger" type="submit">Sign Up</Button>
+                  <Button variant="danger" type="submit">
+                    Add
+                  </Button>
                 </div>
               </Col>
             </Row>
@@ -264,4 +229,4 @@ const SignupForm = () => {
   );
 };
 
-export default SignupForm;
+export default AddUserForm;
