035ae1ad9d34180ded8b21f51feef3f9a63d5ec7
Ah Mar <marzouguiahlem9121@gmail.com>
2023-08-17 13:34:45 +0100

    edit dockerfile and docker-compose

--- Backend/daos/__init__.py
+++ Backend/daos/__init__.py
@@ -1,8 +1,13 @@
 from pymongo import MongoClient
+import os
 
 try:
+    mongo_uri = os.getenv('MONGO_URI')
+    client = MongoClient(mongo_uri)
+
     # client = MongoClient('mongodb://172.16.238.3:27017/') # when you are using docker container
-    client = MongoClient('mongodb://127.0.0.1:27017/') # when you are using your local 
+    # client = MongoClient('mongodb://127.0.0.1:27017/') # when you are using your local 
+
     db = client['mydatabase']
     print('MongoDB connected successfully!')
 except Exception as e:
