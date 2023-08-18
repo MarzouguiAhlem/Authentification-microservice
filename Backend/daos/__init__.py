f21ca6f0e19dfd78ab069317bd82c80720f2f67a
Ah Mar <marzouguiahlem9121@gmail.com>
2023-08-18 18:03:40 +0100

    update dao

--- Backend/daos/__init__.py
+++ Backend/daos/__init__.py
@@ -1,14 +1,17 @@
 from pymongo import MongoClient
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 try:
+
     mongo_uri = os.getenv('MONGO_URI')
     client = MongoClient(mongo_uri)
 
-    # client = MongoClient('mongodb://172.16.238.3:27017/') # when you are using docker container
-    # client = MongoClient('mongodb://127.0.0.1:27017/') # when you are using your local 
-
     db = client['mydatabase']
     print('MongoDB connected successfully!')
+    
 except Exception as e:
     print('Error connecting to MongoDB:', e)
